8c591c369916d0b529bd63f3785429d0f35a563e
denys_matsevych <matsevychd@gmail.com>
2025-04-10 09:51:38 +0300

    configuring the nginx server to use an environment variable for the backend URL

---
+++ frontend/docker-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Replace environment variables in the nginx config
+envsubst '${BACKEND_URL}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+
+# Start nginx
+exec nginx -g 'daemon off;'
